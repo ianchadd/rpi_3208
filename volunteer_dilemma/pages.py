3c4a6f23cc43a86f33b503c407bc4298dd336b0b
Gaby Hall <gabyehall@gmail.com>
2020-04-15 22:55:42 -0400

    Update pages.py

--- volunteer_dilemma/pages.py
+++ volunteer_dilemma/pages.py
@@ -7,17 +7,31 @@ class Introduction(Page):
     pass
 
 
-class Decision(Page):
+class Request_Screen(Page):
     form_model = 'player'
     form_fields = ['volunteer']
 
 
-class ResultsWaitPage(WaitPage):
+class Results_Wait_Page(WaitPage):
     after_all_players_arrive = 'set_payoffs'
+    body_text = "Waiting for other participants to finish."
 
+class Role_Announcement(page):
+    #do the randomly assigned investor thing
+    form_model = 'player'
+    form_fileds = ['volunteer']
+
+class Role_Wait_Page(WaitPage):
+    after_all_players_arrive = 'set_payoffs'
+    body_text = "Waiting for other participants to finish."
+
+class Investment_Decisions(page):
+    timeout_seconds = 60
+    form_model = 'player'
+    form_fields = ['volunteer']
 
 class Results(Page):
     pass
 
 
-page_sequence = [Introduction, Decision, ResultsWaitPage, Results]
+page_sequence = [Introduction, Request_Screen, Results_Wait_Page, Role_Announcement, Role_Wait_Page, Investment_Decisions, Results]
