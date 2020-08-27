f136df2e4786b155a57fa10d5f4f05c0fff63f94
Ian Chadd <chaddi@rpi.edu>
2020-08-27 11:34:53 -0400

    task x part y edits for recipient

--- qsp_dg_recip_intro/pages.py
+++ qsp_dg_recip_intro/pages.py
@@ -7,10 +7,18 @@ class introduction(Page):
     form_model = 'player'
 
     def vars_for_template(self):
-        
+        pvars = self.participant.vars
+        task = pvars['task']
+        part = pvars['part']
+        id_first = pvars['info_treat'] == 'id_first'
         return dict(
-            par_vars = str(self.player.participant.vars)
+            par_vars = str(pvars),
+            task = task,
+            part = part,
+            id_first = id_first
             )
+    def before_next_page(self):
+        self.participant.vars['task'] += 1
 
     def app_after_this_page(self, upcoming_apps):
         if self.participant.vars['info_treat'] == 'id_second':
