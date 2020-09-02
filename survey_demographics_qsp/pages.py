50254899700df2727032d7f7a91e46f93c0d08d1
Ian Chadd <chaddi@rpi.edu>
2020-09-02 14:44:30 -0400

    fixes #241; changes gender error message

--- survey_demographics_qsp/pages.py
+++ survey_demographics_qsp/pages.py
@@ -28,7 +28,7 @@ class Birth(Page):
             self.player.vars_for_template(),
             recip = recip,
         )
-        
+
 class Care(Page):
     form_model='player'
     form_fields = [
@@ -57,10 +57,10 @@ class Gender_SO(Page):
                    'orientation',
                    'other_orientation'
                    ]
-    
+
     def error_message(self,values):
         if values['male'] == 0 and values['female'] == 0 and values['t_male'] == 0 and values['t_female'] == 0 and values['gnc'] == 0 and values['nb'] == 0 and values['other_g'] == 0:
-            return 'You must select at least one response.'
+            return 'You must select at least one response for your gender identity.'
         elif values['other_g'] and type(values['diff_gend']) == type(None):
             return 'If you select Other, you must specify in the provided field'
         elif values['orientation'] =='Other (please state below)' and type(values['other_orientation']) == type(None) :
@@ -115,10 +115,12 @@ class Gen_Demographics(Page):
                    'grew_up_in',
                    'other_grew_up_location'
                    ]
-    
+
     def error_message(self,values):
         if values['relationship'] =='Other (please state below)' and type(values['other_relationship']) == type(None):
             return 'If you select Other, you must specify in the provided field'
+        elif values['white'] == 0 and values['black'] == 0 and values['native'] == 0 and values['asian'] == 0 and values['pacif_island'] == 0 and values['latino'] == 0 and values['arab'] == 0 and values['other_eth'] == 0:
+            return 'You must select at least one response for your ethnicity.'
         elif values['other_eth'] and type(values['other_ethnicity']) == type(None):
             return 'If you select Other, you must specify in the provided field'
         elif values['religion'] =='Some other religious affiliation (please specify below)' and type(values['other_religion']) == type(None):
@@ -202,7 +204,7 @@ class Prolific_Guess(Page):
             self.player.vars_for_template(),
             recip = recip,
         )
-    
+
 
 page_sequence = [Instructions,
                  Birth,
