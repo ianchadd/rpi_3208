d1b266359b0b7a3b44e80cb08c007f5e50ebeed0
Ian Chadd <chaddi@rpi.edu>
2020-09-02 14:32:39 -0400

    fixes #237

--- prolific_ID_begin/models.py
+++ prolific_ID_begin/models.py
@@ -31,3 +31,7 @@ class Player(BasePlayer):
 
     p_ID = models.StringField(label='Paste your Prolific ID here')
 
+    def p_ID_error_message(self,value):
+        if len(value) != 24:
+            return "Your prolific ID must be 24 characters long."
+
